1e3c93919cc0f701f00442c500c3d5f915933abd
halitov-r-k <halitov.r.k@gmail.com>
2025-01-28 23:01:41 +0500

    202501282300

---
+++ 202501282240/BreakContinue.c
@@ -0,0 +1,19 @@
+//Break Continue
+#include<stdio.h>
+int main(){
+int count;
+printf("---Break----\n");
+    for(count = 0; count < 10; count++){
+            printf("%i \n", count);
+            if(count == 5) {break;}
+    }
+    printf("---End-Break---\n");
+    printf("----------Continue-----------\n");
+        for(count = 0; count < 10; count++){
+            if(count == 5){continue;}
+            printf("%i \n", count);
+        }
+
+    printf("------End-Continue-----------");
+return 1;
+}
