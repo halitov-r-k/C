acb8d4b4796a3264cf7a6bd135d021cb96f062a9
halitov-r-k <halitov.r.k@gmail.com>
2025-01-28 16:43:32 +0500

    202501281645

---
+++ 202501281315/loopFor.c
@@ -0,0 +1,13 @@
+//loop for
+#include<stdio.h>
+int main(){
+    printf("---Loop-For---\n");
+    //for(;;){}
+    int count, limit;
+    limit = 5;
+    for(count = 0; count < limit; count++){
+        printf("%i \n", count);
+    }
+    printf("---End-Loop-For---");
+return 0;
+}
