0248abdfc8d14f245b111c6b8f0c85d778c2e03c
halitov-r-k <halitov.r.k@gmail.com>
2025-02-20 07:06:47 +0500

    ADD STEPIK

---
+++ stepik/lesson.c
@@ -0,0 +1,9 @@
+/*Курс Stepik 1*/
+#include<stdio.h>
+#include<locale.h>
+int main(){
+    char *locale = setlocale(LC_ALL, "");
+     //printf("vs code Hello\n");
+    printf(" Русский язык");
+    return 0;
+}
